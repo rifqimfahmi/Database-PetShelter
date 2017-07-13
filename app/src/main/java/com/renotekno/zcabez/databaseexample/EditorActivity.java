7c3f92b092d5eb724fe1964ed4c7f20c5034e29b
Rifqi Mulya Fahmi <rifqi416@gmail.com>
2017-07-13 10:14:11 +0700

    Initial commit

---
+++ app/src/main/java/com/renotekno/zcabez/databaseexample/EditorActivity.java
@@ -0,0 +1,32 @@
+package com.renotekno.zcabez.databaseexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.editor_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.finishAddButton) {
+            Toast.makeText(this, "New pet added", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/renotekno/zcabez/databaseexample/MainActivity.java
@@ -0,0 +1,54 @@
+package com.renotekno.zcabez.databaseexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton addNewPetFAB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+        addNewPetFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.catalog_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_menu_1:
+                Toast.makeText(this, "Action menu 1 clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_menu_2:
+                Toast.makeText(this, "Action menu 2 clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initView() {
+        addNewPetFAB = (FloatingActionButton) findViewById(R.id.addNewPetFAB);
+    }
+}
